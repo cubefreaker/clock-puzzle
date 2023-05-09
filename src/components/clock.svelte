488fd3a378ccf5876be00ab39db747d154140df0
cubefreaker <hamzah.habibi@protonmail.com>
2023-05-09 13:52:39 +0700

    first commit

---
+++ src/components/clock.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let deg;
+    export let index;
+</script>
+
+<div id="clock-{index}" class="relative bg-blue-500 w-96 h-96 rounded-full">
+    {#each [...Array(10).keys()] as i}
+        <div class="absolute w-[20px] h-[10%] top-[40%] left-[49%]" style="transform: rotate({i*30}deg)">TES</div>
+    {/each}
+    <div id="needle-{index}" class="w-[10px] h-[40%] bg-black absolute top-[10%] left-[49%] origin-bottom transition-all" style="transform: rotate({deg}deg)"></div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Clock from '../components/clock.svelte';
+    let deg = 0;
+    let intervalPlus;
+    let intervalMinus;
+
+    function degPlus() {
+        deg += 30;
+        intervalPlus = setInterval(() => {
+            deg += 30;
+        }, 150);
+    }
+
+    function degMinus() {
+        deg -= 30;
+        intervalMinus = setInterval(() => {
+            deg -= 30;
+        }, 150);
+    }
+</script>
+
+<section class="w-screen h-screen bg-teal-200 flex place-items-center justify-center">
+    <div class="top-0 w-screen fixed flex justify-between">
+        <button class="m-4 px-4 py-1 bg-green-500 rounded-lg" on:mousedown={degMinus} on:mouseup={clearInterval(intervalMinus)}>Rotate Left</button>
+        <button class="m-4 px-4 py-1 bg-green-500 rounded-lg" on:mousedown={degPlus} on:mouseup={clearInterval(intervalPlus)}>Rotate Right</button>
+    </div>
+    <Clock index={1} deg={deg}/>
+</section>
